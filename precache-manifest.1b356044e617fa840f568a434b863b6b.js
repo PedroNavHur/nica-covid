ba606b9287be8620929f11061fe6a5268efe740c
Pedro Navarrete <pnavarrete@harding.edu>
2020-05-07 18:58:37 -0600

    Updates

---
+++ precache-manifest.1b356044e617fa840f568a434b863b6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e902cc680eba5da7eb9a5feada4346",
+    "url": "/nica-covid/index.html"
+  },
+  {
+    "revision": "f7cb3fec55d49f328624",
+    "url": "/nica-covid/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b789dd8b78dded30a056",
+    "url": "/nica-covid/static/js/2.76d0545f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nica-covid/static/js/2.76d0545f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cb3fec55d49f328624",
+    "url": "/nica-covid/static/js/main.90a979cc.chunk.js"
+  },
+  {
+    "revision": "82d7c9db4d6dcf55ef0c",
+    "url": "/nica-covid/static/js/runtime-main.d4e9c682.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nica-covid/static/media/logo.5d5d9eef.svg"
+  }
+]);
